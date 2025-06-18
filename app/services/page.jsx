80f7fa1e0797575f684964e6e677ae7cfa2072e3
MOHAMED AFIFI <mada2010545@gmail.com>
2025-06-18 23:06:50 +0300

    semi finished

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,41 @@
 "use client";
 import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import Image from "next/image";
+import { MdArrowRightAlt, MdOutlineArrowOutward } from "react-icons/md";
+
+const services = [
+  {
+    icon: "/assets/services/design.svg",
+    href: "",
+    title: "Website Interface Design",
+  },
+  {
+    icon: "/assets/services/frontend.svg",
+    href: "",
+    title: "Frontend Development",
+  },
+  {
+    icon: "/assets/services/backend.svg",
+    href: "",
+    title: "Backend Development",
+  },
+  {
+    icon: "/assets/services/seo.svg",
+    href: "",
+    title: "Search Engine Optimization",
+  },
+  {
+    icon: "/assets/services/video.svg",
+    href: "",
+    title: "Video Editing",
+  },
+];
 
 export default function Services() {
   return (
@@ -15,8 +51,56 @@ export default function Services() {
           ease: "easeIn",
         },
       }}
+      className="h-screen flex items-center"
     >
-      Services
+      <div className="container mx-auto w-full flex flex-col gap-16 ">
+        <div className="flex flex-col xl:flex-row justify-between items-start xl:items-center gap-8">
+          <h2 className="h2 max-w-[480px] text-left xl:mb-0  ">
+            Custom <span className="text-accent">Web Solutions</span> To Boost
+            Your Business
+          </h2>
+          <button className="btn btn-lg btn-accent flex gap-2">
+            All Services <MdArrowRightAlt className="text-2xl" />
+          </button>
+        </div>
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          modules={[Pagination, Autoplay]}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 2500,
+            pauseOnMouseEnter: true,
+          }}
+          loop={true}
+          className="h-[320px]"
+        >
+          {services.map((service, index) => (
+            <SwiperSlide key={index}>
+              <div className="bg-secondary/90 w-full h-[284px] rounded-[20px] px-[30px] py-[40px] flex flex-col justify-between ">
+                <div className="flex items-center justify-between mb-12 ">
+                  <Image
+                    src={service.icon}
+                    alt={service.title}
+                    width={48}
+                    height={48}
+                  />
+                  <div className="w-12 h-12 bg-accent rounded-full flex items-center justify-center cursor-pointer text-2xl hover:rotate-45 transition-all ">
+                    <MdOutlineArrowOutward />
+                  </div>
+                </div>
+                <h5 className="text-[22px] font-medium max-w-[240px]">
+                  {service.title}
+                </h5>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </motion.section>
   );
 }
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,69 @@
 "use client";
 import { motion } from "framer-motion";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import Link from "next/link";
+import Image from "next/image";
+import { MdArrowOutward } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+
+const projects = [
+  {
+    id: 1,
+    category: "Frontend",
+    title: "The Wild Oasis Website",
+    description: "A modern website for a fictional travel agency.",
+    image: "/assets/work/thumb1.png",
+    link: "https://wild-oasis-website-afifi.netlify.app",
+    github: "",
+    tech: ["React", "Next.js", "Tailwind CSS"],
+  },
+  {
+    id: 2,
+    category: "Frontend",
+    title: "The ",
+    description: "A modern website for a fictional travel agency.",
+    image: "/assets/work/thumb1.png",
+    link: "https://the-wild-oasis.com",
+    github: "",
+    tech: ["React", "Next.js", "Tailwind CSS"],
+  },
+  {
+    id: 3,
+    category: "Frontend",
+    title: "The Wild ",
+    description: "A modern website for a fictional travel agency.",
+    image: "/assets/work/thumb1.png",
+    link: "https://the-wild-oasis.com",
+    github: "",
+    tech: ["React", "Next.js", "Tailwind CSS"],
+  },
+  {
+    id: 4,
+    category: "Full-stack",
+    title: " Oasis Website",
+    description: "A modern website for a fictional travel agency.",
+    image: "/assets/work/thumb1.png",
+    link: "https://the-wild-oasis.com",
+    github: "",
+    tech: ["React", "Next.js", "Tailwind CSS"],
+  },
+  {
+    id: 5,
+    category: "Full-stack",
+    title: " Website",
+    description: "A modern website for a fictional travel agency.",
+    image: "/assets/work/thumb1.png",
+    link: "https://the-wild-oasis.com",
+    github: "",
+    tech: ["React", "Next.js", "Tailwind CSS"],
+  },
+];
+
+const categories = ["Frontend", "Full-stack"];
 
 export default function Work() {
   return (
@@ -15,8 +79,94 @@ export default function Work() {
           ease: "easeIn",
         },
       }}
+      className="min-h-screen flex items-center py-24 xl:py-0 "
     >
-      Work
+      <div className="container mx-auto w-full h-full flex flex-col justify-center ">
+        <h2 className="h2 mb-6 xl:mb-12 max-w-[600px] ">
+          My Latest <span className="text-accent">Projects</span>
+        </h2>
+        <Tabs
+          defaultValue="Frontend"
+          className="w-full flex flex-col gap-6 xl:gap-12 "
+        >
+          <TabsList className="flex flex-wrap justify-center items-center gap-4 h-full mb-4 xl:mb-0">
+            {categories.map((category) => (
+              <TabsTrigger
+                key={category}
+                value={category}
+                className="capitalize border border-white/20 data-[state=active]:bg-accent data-[state=active]:border-accent h-[48px] px-6 rounded-full cursor-pointer "
+              >
+                {category}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <div className="h-[400px] scrollbar scrollbar-thumb-accent scrollbar-track-accent/5 overflow-y-scroll xl:overflow-y-visible ">
+            {categories.map((category) => {
+              return (
+                <TabsContent key={category} value={category}>
+                  <Swiper
+                    modules={[Pagination]}
+                    pagination={{
+                      clickable: true,
+                      dynamicBullets: true,
+                    }}
+                    className="h-max xl:h-[460px] "
+                  >
+                    {projects
+                      .filter((project) => project.category === category)
+                      .map((project) => (
+                        <SwiperSlide key={project.id} className="h-full ">
+                          <div className="flex flex-col xl:flex-row gap-8 xl:gap-12 mx-5">
+                            <div className="w-full max-w-[350px] flex flex-col gap-6 xl:gap-8 xl:pt-6 order-2 xl:order-none ">
+                              <h3 className="h3">{project.title}</h3>
+                              <div className="xl:mb-4 max-w-[300px] min-h-[130px] ">
+                                <p className="mb-4">Technologies Used</p>
+                                <ul className="flex flex-wrap gap-4 ">
+                                  {project.tech.map((item, index) => {
+                                    return (
+                                      <li
+                                        key={index}
+                                        className="flex items-center gap-4 bg-[#a883ff]/13 h-[28px] px-[14px] rounded-full "
+                                      >
+                                        {item}
+                                      </li>
+                                    );
+                                  })}
+                                </ul>
+                              </div>
+                              <div className="flex flex-col sm:flex-row gap-4 items-start ">
+                                <Link href={project.link}>
+                                  <button className="btn btn-sm btn-accent flex gap-2 ">
+                                    <MdArrowOutward className="text-lg " />
+                                    <span>Live Project</span>
+                                  </button>
+                                </Link>
+                                <Link href={project.github}>
+                                  <button className="btn btn-sm btn-white flex gap-2 ">
+                                    <FaGithub className="text-lg " />
+                                    <span>Github Repo</span>
+                                  </button>
+                                </Link>
+                              </div>
+                            </div>
+                            <div className="w-full h-[200px] md:h-[300px] xl:h-[400px] relative order-1 xl:order-none rounded-lg overflow-hidden  ">
+                              <Image
+                                src={project.image}
+                                alt={project.title}
+                                fill
+                                className="object-center"
+                              />
+                            </div>
+                          </div>
+                        </SwiperSlide>
+                      ))}
+                  </Swiper>
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
     </motion.section>
   );
 }
