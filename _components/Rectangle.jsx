eae61d70c1875c7e9bbbe5e081e83bfed250ff36
MOHAMED AFIFI <mada2010545@gmail.com>
2025-06-19 19:55:17 +0300

    speeded

--- _components/Rectangle.jsx
+++ _components/Rectangle.jsx
@@ -27,7 +27,7 @@ export default function Rectangle() {
         exit="exit"
         transition={{
           delay: 0,
-          duration: 0.4,
+          duration: 0.2,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-30 bg-[#1b162b]"
@@ -38,8 +38,8 @@ export default function Rectangle() {
         animate="animate"
         exit="exit"
         transition={{
-          delay: 0.15,
-          duration: 0.6,
+          delay: 0.1,
+          duration: 0.4,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-20 bg-[#241e36]"
@@ -51,8 +51,8 @@ export default function Rectangle() {
         animate="animate"
         exit="exit"
         transition={{
-          delay: 0.35,
-          duration: 0.8,
+          delay: 0.2,
+          duration: 0.6,
           ease: [0.63, 0, 0.17, 1],
         }}
         className="fixed top-full w-screen h-screen z-10 bg-[#2e2743]"
