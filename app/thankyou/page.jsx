f0de7b294cf42f3094780cd856c675766b763775
MOHAMED AFIFI <mada2010545@gmail.com>
2025-06-19 15:25:25 +0300

    thank you

--- app/thankyou/page.jsx
+++ app/thankyou/page.jsx
@@ -1,12 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Page() {
   return (
-    <div className="flex flex-col h-[500px] gap-10 justify-center items-center text-center space-y-6 mt-4">
-      <h1 className="text-3xl font-semibold">Thank you for your Message!</h1>
-      <Link href="/" className="underline text-xl text-accent-500 inline-block">
-        Back to Homepage &rarr;
-      </Link>
-    </div>
+    <motion.section
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 1,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+    >
+      <div className="flex flex-col h-[500px] gap-10 justify-center items-center text-center space-y-6 mt-4">
+        <h1 className="text-3xl font-semibold">Thank you for your Message!</h1>
+        <Link
+          href="/"
+          className="underline text-xl text-accent-500 inline-block"
+        >
+          Back to Homepage &rarr;
+        </Link>
+      </div>
+    </motion.section>
   );
 }
